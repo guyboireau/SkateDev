6d09d0018c400d89b6715a04177e53a2f8d8cbd4
guyboireau <boireauguy@gmail.com>
2023-12-15 17:11:53 +0100

    petit oublie

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,10 +1,11 @@
 const Footer = () => {
-    return (
-      <footer>
-        <p>© 2022 Skater Report</p>
-        <p>Nous contacter</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer>
+      <p>© {new Date().getFullYear()} Skater Report</p>
+      <a href="mailto:skaterreport@gmail.com "> Nous contacter</a>
+
+    </footer>
+  );
+}
+
+export default Footer;
